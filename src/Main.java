ebc2d823bbaa961b96e63e2aea9573a2970197aa
Dail-Jonker <u13325630@tuks.co.za>
2018-10-09 22:20:43 +0200

    Read from file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.io.*;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        AsciiAlphabet alphabet = new AsciiAlphabet();
+      String str = "";
+      String message = "";
+      try
+      {
+        BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+        while((str = br.readLine()) != null)
+          message += str;
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      };
+      AsciiAlphabet alphabet = new AsciiAlphabet();
     }
 }
