e2c61ce6f08a39755d514f1effc84688cc1b3063
Brendan <brendanbath@gmail.com>
2018-10-09 22:57:42 +0200

    Added helper functions to the alphabet

--- src/Alphabet.java
+++ src/Alphabet.java
@@ -3,6 +3,6 @@ public interface Alphabet
     public int getIndex(char input);
     public char getChar(int index);
     public int getLength();
-    public char getCharWithOffset(char character,int offset);
+    public char getCharWithOffset(char character,int offset) throws IllegalArgumentException;
     public int getDifference(char c1, char c2) throws IllegalArgumentException;
 }
